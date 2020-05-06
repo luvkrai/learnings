5c575c364daa858957e2cf1147d7be93ef531d09
luvkrai <43914785+luvkrai@users.noreply.github.com>
2020-05-07 02:07:41 +0530

    Update ilo.py

--- ilo.py
+++ ilo.py
@@ -1,7 +1,7 @@
 #!/bin/python
 import redfish
 
-REST_OBJ = redfish.redfish_client(base_url="https://uls-ep-swtools-04-m1.wdc.com",username="it-ops-adm",
+REST_OBJ = redfish.redfish_client(base_url="https://uls-ep-swtools-04-m1.luv.com",username="it-ops-adm",
                              password="$vc@!t0p$", default_prefix='/rest/v1')
 REST_OBJ.login(auth="session")
 #response = REST_OBJ.get("/rest/v1/systems/1", None)
