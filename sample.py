ffa5288e50e022595c8825595fc0a6c7ce483412
luvkrai <43914785+luvkrai@users.noreply.github.com>
2021-01-05 21:29:23 +0530

    Delete sample.py

--- sample.py
+++
@@ -1,8 +0,0 @@
-from TestRail import TestRail
-
-
-connection = TestRail(server='http://qa-testrail1.wdc.com',
-                              user='jenkins@qafusionio.com',
-                              password='Centcio1')
-							  
-#print(connection)
