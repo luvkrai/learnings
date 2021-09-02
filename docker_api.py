0234acb207e9f5daf77ecc87392b70b3420582be
luvkrai <43914785+luvkrai@users.noreply.github.com>
2021-09-02 15:47:43 +0530

    Update docker_api.py

--- docker_api.py
+++ docker_api.py
@@ -2,7 +2,7 @@
 # Go to http://containertutorials.com/py/docker-py.html
 import docker
 #cli = docker.APIClient(base_url='tcp://uls-ep-swtools-03.wdc.com:2375', tls=False)
-cli = docker.APIClient(base_url='tcp://uls-ep-essd25.wdc.com:2375', tls=False)
+cli = docker.APIClient(base_url='tcp://uls-ep-essd25:2375', tls=False)
 print cli.containers()
 container = cli.create_container(image='ubuntu:latest', command='/bin/sleep 30')
 print container
